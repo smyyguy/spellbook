1590f8af736e5d895719fe968651ab6559329f72
Dan Smith | Blockworks Research <119909738+smyyguy@users.noreply.github.com>
2024-10-06 20:10:18 -0400

    Update staking_solana_validator_stake_account_epochs_raw.sql
    I noticed the lookback was bumped from 2 to 7 days here, but it seems the same issue where new epoch are not recognized is still occurring. I think it needs a full refresh every time in that case

--- dbt_subprojects/solana/models/_sector/staking/solana/staking_solana_validator_stake_account_epochs_raw.sql
+++ dbt_subprojects/solana/models/_sector/staking/solana/staking_solana_validator_stake_account_epochs_raw.sql
@@ -1,39 +1,31 @@
 {{ config(
     schema = 'staking_solana'
-    , alias = 'validator_stake_account_epochs_raw'
-    , materialized = 'incremental'
-    , file_format = 'delta'
-    , incremental_strategy = 'merge'
-    , unique_key = ['epoch', 'epoch_time', 'stake_account', 'vote_account']
+  , alias = 'validator_stake_account_epochs_raw'
+  , materialized = 'table'
+  , file_format = 'delta'
+) }}
+
+with base as (
+    SELECT 
+        epoch.epoch
+      , epoch.block_time as epoch_time
+      , epoch.epoch_start_slot
+      , epoch.epoch_next_start_slot
+      , vote.stake_account
+      , max_by(vote.vote_account, vote.block_time) as vote_account
+    FROM {{ ref('staking_solana_stake_account_delegations') }} vote
+    LEFT JOIN {{ ref('solana_utils_epochs') }} epoch
+        ON first_block_epoch = true
+    WHERE vote.block_slot < epoch.block_slot
+    GROUP BY 1,2,3,4,5
 )
-}}
 
---don't expose this table in DE
-with 
-    base as (
-        SELECT 
-            epoch.epoch
-            , epoch.block_time as epoch_time
-            , epoch.epoch_start_slot
-            , epoch.epoch_next_start_slot
-            , vote.stake_account
-            , max_by(vote.vote_account, vote.block_time) as vote_account --latest delegated vote before epoch started. then get the rewards from accounts per epoch.
-        FROM {{ ref('staking_solana_stake_account_delegations') }} vote
-        LEFT JOIN {{ ref('solana_utils_epochs') }} epoch
-            ON first_block_epoch = true --cross join
-            {% if is_incremental() %}
-            AND epoch.block_time >= date_trunc('day', now() - interval '7' day)
-            {% endif %}
-        WHERE vote.block_slot < epoch.block_slot --only get changes to accounts before start of epoch
-        GROUP BY 1,2,3,4,5
-    )
-
-SELECT
+SELECT 
     epoch
-    , epoch_time
-    , epoch_start_slot
-    , epoch_next_start_slot
-    , stake_account
-    , vote_account
+  , epoch_time
+  , epoch_start_slot
+  , epoch_next_start_slot
+  , stake_account
+  , vote_account
 FROM base
 WHERE vote_account is not null
